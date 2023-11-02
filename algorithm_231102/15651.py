c4dbabdb6622af0a20b3fb1220edd2273f5e8f91
kimtaeho <ddsdl@naver.com>
2023-11-02 14:00:30 +0900

    수정

---
+++ algorithm_231102/15651.py
@@ -0,0 +1,7 @@
+import sys
+n, m = int(sys.stdin.readline().split())
+num = []
+
+def dfs(n, m):
+    if len(num) == m:
+                   
--- algorithm_231102/leet_179.py
+++ algorithm_231102/leet_179.py
@@ -8,6 +8,8 @@ class Solution:
                 if nums[i] + nums[j] < nums[j] + nums[i]:
                     nums[i], nums[j] = nums[j], nums[i]
 
+        if nums[0] == '0':
+            return '0'
         return ''.join(nums)
 
 
